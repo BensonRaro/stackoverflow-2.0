18c4d7901e5fe2a06fc7cf607c2b3c7074a6dfa6
BensonRaro <bensonraro22@gmail.com>
2024-07-14 00:52:17 +0300

    votes update

--- components/Votes.tsx
+++ components/Votes.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { formatAndDivideNumber } from "@/lib/utils";
 import Image from "next/image";
-import { usePathname, useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useTransition } from "react";
+import { downvote, saved, upvote } from "@prisma/client";
+
+import { formatAndDivideNumber } from "@/lib/utils";
 import { toast } from "@/components/ui/use-toast";
 import { UnSave, save } from "@/actions/Save";
 import { UnUpvote, Upvotefn } from "@/actions/Upvote";
 import { DeleteDownVote, DownVote } from "@/actions/DownVote";
-import { downvote, saved, upvote } from "@prisma/client";
+import { Button } from "@/components/ui/button";
 
 interface Props {
   type: "answer" | "question";
@@ -35,23 +36,31 @@ const Votes = ({
   Downvotes,
   Upvotes,
 }: Props) => {
-  const pathname = usePathname();
-  const router = useRouter();
-
+  const [pending, startTranstion] = useTransition();
   const hasSaved = saved?.some(
     (item) => item.userId === userId && item.questionId === itemId
   );
   // console.log(hasSaved);
   const use = type === "answer" ? "answer" : "question";
-  const handleSave = async () => {
-    if (hasSaved) {
-      const SavedByUser = saved?.find(
-        (item) => item.userId === userId && item.questionId === itemId
-      );
-      await UnSave(SavedByUser?.id);
-    } else {
-      await save(itemId);
-    }
+  const handleSave = () => {
+    startTranstion(async () => {
+      if (hasSaved) {
+        const SavedByUser = saved?.find(
+          (item) => item.userId === userId && item.questionId === itemId
+        );
+        await UnSave(SavedByUser?.id)
+          .then(() => {})
+          .catch((error) => {
+            console.log(error);
+          });
+      } else {
+        await save(itemId)
+          .then(() => {})
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    });
 
     return toast({
       title: `Question ${
@@ -61,29 +70,34 @@ const Votes = ({
     });
   };
 
-  const handleVote = async (action: "upvote" | "downvote") => {
+  const handleVote = (action: "upvote" | "downvote") => {
     if (!userId) {
       return toast({
         title: "Please log in",
         description: "You must be logged in to perform this action",
       });
     }
-
     if (action === "upvote") {
       if (!hasupVoted) {
-        if (hasdownVoted) {
-          const DownvotesResults = Downvotes?.find(
+        startTranstion(async () => {
+          if (hasdownVoted) {
+            const DownvotesResults = Downvotes?.find(
+              (item) => item.userId === userId
+            );
+            await DeleteDownVote(DownvotesResults?.id);
+
+            await Upvotefn(itemId, use);
+          } else {
+            await Upvotefn(itemId, use);
+          }
+        });
+      } else {
+        startTranstion(async () => {
+          const upvotesResults = Upvotes?.find(
             (item) => item.userId === userId
           );
-          await DeleteDownVote(DownvotesResults?.id);
-
-          await Upvotefn(itemId, use);
-        } else {
-          await Upvotefn(itemId, use);
-        }
-      } else {
-        const upvotesResults = Upvotes?.find((item) => item.userId === userId);
-        await UnUpvote(upvotesResults?.id);
+          await UnUpvote(upvotesResults?.id);
+        });
       }
       return toast({
         title: `Upvote ${!hasupVoted ? "Successful" : "Removed"}`,
@@ -93,22 +107,26 @@ const Votes = ({
 
     if (action === "downvote") {
       if (!hasdownVoted) {
-        if (hasupVoted) {
-          const upvotesResults = Upvotes?.find(
+        startTranstion(async () => {
+          if (hasupVoted) {
+            const upvotesResults = Upvotes?.find(
+              (item) => item.userId === userId
+            );
+            await UnUpvote(upvotesResults?.id);
+
+            await DownVote(itemId, use);
+          } else {
+            await DownVote(itemId, use);
+          }
+        });
+      } else {
+        startTranstion(async () => {
+          const DownvotesResults = Downvotes?.find(
             (item) => item.userId === userId
           );
-          await UnUpvote(upvotesResults?.id);
-          await DownVote(itemId, use);
-        } else {
-          await DownVote(itemId, use);
-        }
-      } else {
-        const DownvotesResults = Downvotes?.find(
-          (item) => item.userId === userId
-        );
-        await DeleteDownVote(DownvotesResults?.id);
+          await DeleteDownVote(DownvotesResults?.id);
+        });
       }
-
       return toast({
         title: `Downvote ${!hasdownVoted ? "Successful" : "Removed"}`,
         variant: !hasdownVoted ? "default" : "destructive",
@@ -120,18 +138,19 @@ const Votes = ({
     <div className="flex gap-5">
       <div className="flex-center gap-2.5">
         <div className="flex-center gap-1.5">
-          <Image
-            src={
-              hasupVoted
-                ? "/assets/icons/upvoted.svg"
-                : "/assets/icons/upvote.svg"
-            }
-            width={18}
-            height={18}
-            alt="upvote"
-            className="cursor-pointer"
-            onClick={() => handleVote("upvote")}
-          />
+          <Button onClick={() => handleVote("upvote")} disabled={pending}>
+            <Image
+              src={
+                hasupVoted
+                  ? "/assets/icons/upvoted.svg"
+                  : "/assets/icons/upvote.svg"
+              }
+              width={18}
+              height={18}
+              alt="upvote"
+              className="cursor-pointer"
+            />
+          </Button>
 
           <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
             <p className="subtle-medium text-dark400_light900">
@@ -141,18 +160,19 @@ const Votes = ({
         </div>
 
         <div className="flex-center gap-1.5">
-          <Image
-            src={
-              hasdownVoted
-                ? "/assets/icons/downvoted.svg"
-                : "/assets/icons/downvote.svg"
-            }
-            width={18}
-            height={18}
-            alt="downvote"
-            className="cursor-pointer"
-            onClick={() => handleVote("downvote")}
-          />
+          <Button onClick={() => handleVote("downvote")} disabled={pending}>
+            <Image
+              src={
+                hasdownVoted
+                  ? "/assets/icons/downvoted.svg"
+                  : "/assets/icons/downvote.svg"
+              }
+              width={18}
+              height={18}
+              alt="downvote"
+              className="cursor-pointer"
+            />
+          </Button>
 
           <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
             <p className="subtle-medium text-dark400_light900">
@@ -163,18 +183,19 @@ const Votes = ({
       </div>
 
       {type === "question" && (
-        <Image
-          src={
-            hasSaved
-              ? "/assets/icons/star-filled.svg"
-              : "/assets/icons/star-red.svg"
-          }
-          width={18}
-          height={18}
-          alt="star"
-          className="cursor-pointer"
-          onClick={handleSave}
-        />
+        <Button disabled={pending} onClick={handleSave}>
+          <Image
+            src={
+              hasSaved
+                ? "/assets/icons/star-filled.svg"
+                : "/assets/icons/star-red.svg"
+            }
+            width={18}
+            height={18}
+            alt="star"
+            className="cursor-pointer"
+          />
+        </Button>
       )}
     </div>
   );
