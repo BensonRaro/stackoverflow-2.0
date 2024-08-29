3518795f999b2a14872275b7217d268b97caf0bf
BensonRaro <bensonraro22@gmail.com>
2024-08-29 22:03:16 +0300

    update

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
 
 import TagCard from "@/components/TagCard";
@@ -17,18 +18,32 @@ const RightSidebar = async () => {
   // const userTags = Tags.filter((tag) => tag.questionId === "");
   // console.log(Tags);
   return (
-    <div className="dark:dark-gradient bg-light-800 custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
-      <h1 className="h3-bold text-dark200_light900">Your Tags</h1>
-      {Tags.length === 0 && (
-        <div className="text-neutral-400 mt-2">You have No Tags</div>
-      )}
-      <AddTag />
-      <div className="flex flex-col gap-2 mt-2">
-        {Tags.map((tag) => (
-          <TagCard key={tag.id} tag={tag.tag} id={tag.id} use="RightSidebar" />
-        ))}
-      </div>
-    </div>
+    <>
+      <SignedOut>
+        <h2 className="text-neutral-400 text-xl font-semibold mt-4">
+          Sign In to Add Your Tags
+        </h2>
+      </SignedOut>
+      <SignedIn>
+        <div className="dark:dark-gradient bg-light-800 custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
+          <h1 className="h3-bold text-dark200_light900">Your Tags</h1>
+          {Tags.length === 0 && (
+            <div className="text-neutral-400 mt-2">You have No Tags</div>
+          )}
+          <AddTag />
+          <div className="flex flex-col gap-2 mt-2">
+            {Tags.map((tag) => (
+              <TagCard
+                key={tag.id}
+                tag={tag.tag}
+                id={tag.id}
+                use="RightSidebar"
+              />
+            ))}
+          </div>
+        </div>
+      </SignedIn>
+    </>
   );
 };
 
