12d64612e3fc7603bc19568299edea88c5f1ef4a
BensonRaro <bensonraro22@gmail.com>
2024-08-29 22:08:00 +0300

    update

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -20,9 +20,11 @@ const RightSidebar = async () => {
   return (
     <>
       <SignedOut>
-        <h2 className="text-neutral-400 text-xl font-semibold mt-4">
-          Sign In to Add Your Tags
-        </h2>
+        <div className="dark:dark-gradient bg-light-800 custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
+          <h2 className="text-neutral-400 text-xl font-semibold mt-4 items-center justify-center flex">
+            Sign In to Add Your Tags
+          </h2>
+        </div>
       </SignedOut>
       <SignedIn>
         <div className="dark:dark-gradient bg-light-800 custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
